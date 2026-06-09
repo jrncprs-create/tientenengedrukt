58a43de6012e50910a3782f1bb4c84008f0908ba
jrncprs-create <jrncprs@gmail.com>
2026-06-09 12:41:58 +0200

    Refine project index hover layout

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import type { Project } from "@/data/projects";
 import { ScrollReveal } from "./ScrollReveal";
@@ -7,12 +8,19 @@ type ProjectCardProps = {
 };
 
 export function ProjectCard({ project }: ProjectCardProps) {
+  const previewImage = project.images[0];
+
   return (
     <ScrollReveal className="project-row">
       <Link href={`/work/${project.slug}`} className="project-link">
-        <span className="project-title">{project.title}</span>
-        <span className="project-meta">
-          {project.category} - {project.year}
+        <span className="project-line">
+          <span className="project-title">{project.title}</span>
+          <span className="project-meta">
+            {project.category} / {project.year}
+          </span>
+        </span>
+        <span className="project-preview" aria-hidden="true">
+          <Image src={previewImage} alt="" width={1600} height={420} />
         </span>
       </Link>
     </ScrollReveal>
